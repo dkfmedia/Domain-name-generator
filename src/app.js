4ca0db4668e8b8fe599bb256a38eb4c85d5e046e
Dana M <147849932+dkfmedia@users.noreply.github.com>
2023-12-07 01:57:10 +0000

    basic domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let containerselect = document.querySelector(".Domains");
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+      for (let k = 0; k < 2; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        let newChild = document.createElement("h3");
+        newChild.innerText = pronoun[i] + adj[j] + noun[k] + ".com";
+        containerselect.append(newChild);
+      }
+    }
+  }
 };
