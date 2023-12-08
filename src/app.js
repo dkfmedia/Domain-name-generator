5a7439349450f9e6e1beddd50e058e03099752a1
Dana M <147849932+dkfmedia@users.noreply.github.com>
2023-12-08 02:07:11 +0000

    added btn and functionality

--- src/app.js
+++ src/app.js
@@ -7,16 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#generator").innerHTML = generateDomain();
+};
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+const shuffleArray = array => {
+  const shuffledArray = array.slice();
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+};
+
+const generateDomain = () => {
+  let pronoun = shuffleArray(["the", "our"]);
+  let adj = shuffleArray(["great", "big"]);
+  let noun = shuffleArray(["jogger", "racoon"]);
 
   let containerselect = document.querySelector(".Domains");
+  containerselect.innerHTML = "";
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < 2; j++) {
       for (let k = 0; k < 2; k++) {
         console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+
         let newChild = document.createElement("h3");
         newChild.innerText = pronoun[i] + adj[j] + noun[k] + ".com";
         containerselect.append(newChild);
@@ -24,3 +40,10 @@ window.onload = function() {
     }
   }
 };
+
+const reloadBtn = document.querySelector("#btn");
+const reload = () => {
+  generateDomain();
+};
+
+reloadBtn.addEventListener("click", reload);
